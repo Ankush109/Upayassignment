f719039a9d5d1a56c8464802575aff0228b04231
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2022-07-28 02:24:08 +0530

    basic layput and product card done

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+function Navbar() {
+  return (
+    <div className="nav">
+      <div className="header">
+        <h2>UPayments</h2>
+      </div>
+      <div className="btn">
+        <h3>Register</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Products/Products.js
@@ -0,0 +1,124 @@
+import React from "react";
+import "./products.css";
+function Products() {
+  return (
+    <div className="products">
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+      <div className="product-container">
+        <div className="img">
+          <img src="https://www.cetmen.com.tr/carola-plus-tekli-koltuk-140046-tekli-koltuk-cetmen-664-42-B.png" />
+        </div>
+        <div className="product-name">chair</div>
+        <div className="product-price">$300</div>
+      </div>
+    </div>
+  );
+}
+
+export default Products;
